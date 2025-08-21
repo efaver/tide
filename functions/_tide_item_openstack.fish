370827a1d3b41087b982f8b1eea88a8ef3fd9974
Enrico Favero <enrico.favero@ecmwf.int>
2025-08-21 16:50:39 +0200

    refactor _tide_item_openstack.fish

--- functions/_tide_item_openstack.fish
+++ functions/_tide_item_openstack.fish
@@ -1,15 +1,23 @@
 function _tide_item_openstack
-    # set up openstack version var
-    if test -n "$OS_AUTH_URL"; and test -z "$OS_VERSION"
-        if type -q curl; and type -q jq
-            set OS_VERSION "openstack " (curl -q $OS_AUTH_URL |jq '.[].values[].id[]')
-        else
-            set OS_VERSION "openstack"
-        end
+    # exit if no endpoint is set
+    if test -z "$OS_AUTH_URL"
+        return
     end
-    # it needs to be cleared somehow TODO
 
+    # set up openstack api version
+    if type -q curl; and type -q jq
+        set OS_API_VERSION (curl -qs $OS_AUTH_URL 2>/dev/null |jq '.[].values[].id')
+    end
+
+    # setup  openstack info var
+    if test -n "$OS_NAME"; and test -n "$OS_API_VERSION"
+        set OS_INFO " (" $OS_NAME-$OS_API_VERSION ")"
+    else
+        set OS_INFO " (" $OS_NAME $OS_API_VERSION ")"
+    end
+
+    # print prompt
     if test -n "$OS_USERNAME"; and test -n "$OS_PROJECT_NAME"
-        _tide_print_item openstack $tide_openstack_icon' ' "$OS_VERSION " "$OS_USERNAME@$OS_PROJECT_NAME"
+        _tide_print_item openstack $tide_openstack_icon " " $OS_USERNAME@$OS_PROJECT_NAME $OS_INFO
     end
 end
